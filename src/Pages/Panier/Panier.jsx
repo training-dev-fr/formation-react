832cb13cf41e7fe50b8f019f7d0e73a004523b70
training-dev <contact@training-dev.fr>
2022-11-18 17:17:47 +0100

    ajout readme + homepage

--- src/Pages/Panier/Panier.jsx
+++ src/Pages/Panier/Panier.jsx
@@ -3,6 +3,7 @@ import "./Panier.css";
 
 export default function Panier({panier,addBasket}){
     return(
+        
         <div className="panier">
             {
                 panier.map(product => <Product product={product} addBasket={addBasket} type="basket" ></Product>)
